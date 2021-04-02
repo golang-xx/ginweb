0503e04f77d34f34e8874735d5a704ad28bbfbcc
liliang8858 <liliang8858@126.com>
2021-04-02 21:26:54 +0800

    update

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 func main(){
 	r := gin.Default()
 	r.GET("/",func(c *gin.Context){
-		c.JSON(200,gin.H{"message":"pong",})
+		c.JSON(200,gin.H{"message":"pong1",})
 	})
 	r.Run(":8092")
 }
