b9d24df11170d074daa599dcd9fdaeedf2eb7f09
liliang8858 <liliang8858@126.com>
2021-04-11 22:15:39 +0800

    update

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong6"})
-	})
+	NewUserController(r).Router()
 	r.Run(":8092")
 }
